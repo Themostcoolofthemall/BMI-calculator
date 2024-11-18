93f62ffbad950455f32a6e356807203a0b56c8cd
Themostcoolofthemall <nidhijuneja.dcmp@gmail.com>
2024-11-18 18:20:33 +0000

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator by Anantam")
+bill = float(input("How much money did you spend today"))
+print("Your M.O.M is made of moneyyyyyyyyyyyyyy")
+tip = int(input("How much % are you willing to give, dont worry, I wont judge you"))
+finalbill = bill+ (bill*tip)/100
+print(finalbill)
+
+
